b3d0aac0579b92199a75a6621dcc60ec135d84f6
Josué Camelo <josueprg@gmail.com>
2017-12-23 15:52:53 -0200

    Property Bindings

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
+import {Task} from "../task";
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
+
 export class TaskListComponent {
-
-
   tasks = [];
-  task = "";
-
+  task:Task = {
+    name: "",
+    valor: 0
+  };
   add(){
-    this.tasks.push(this.task);
+    let task = Object.assign({}, this.task); //adicionando objeto vario no
+    this.tasks.push(task);
   }
-
 }
